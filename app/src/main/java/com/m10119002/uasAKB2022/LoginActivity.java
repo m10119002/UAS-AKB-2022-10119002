5254a72416a6fa1fc57b2acb0aa211b8856a802f
m10119002 <72883370+m10119002@users.noreply.github.com>
2022-08-14 03:28:54 +0700

    Login - Tampilan

---
+++ app/src/main/java/com/m10119002/uasAKB2022/LoginActivity.java
@@ -0,0 +1,14 @@
+package com.m10119002.uasAKB2022;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class LoginActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+    }
+}
